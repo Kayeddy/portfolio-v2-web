ba7c047b622a74e5e36142ee9765cd0b17f19f39
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2024-03-28 15:36:03 -0500

    refactor(contact): Update contact form to adequately send emails to my email address on form submission

--- src/components/homepage/Contact.jsx
+++ src/components/homepage/Contact.jsx
@@ -6,9 +6,11 @@ import { useEffect, useState, useRef } from "react";
 import { ScrollTrigger } from "gsap/all";
 import { gsap } from "gsap";
 import Heading from "../ui/Heading";
+import { useForm, ValidationError } from "@formspree/react";
 
 export default function Contact({ hashRef }) {
   const [time, setTime] = useState(new Date().toLocaleTimeString());
+  const [state, handleSubmit] = useForm("mqkrybzv");
 
   const heading = useRef(null);
   const body = useRef(null);
@@ -54,104 +56,125 @@ export default function Contact({ hashRef }) {
       <Heading title="Contact" />
       <div
         ref={contactSection}
-        className="flex flex-col gap-20 mt-10 md:grid md:grid-cols-6 md:px-12"
+        className="mt-10 flex flex-col gap-20 md:grid md:grid-cols-6 md:px-12"
       >
         <div className="col-span-4">
           <h3
             ref={heading}
-            className="max-w-lg font-semibold leading-tight translate-y-10 opacity-0 text-heading-3 2xl:max-w-3xl 2xl:text-5xl"
+            className="max-w-lg translate-y-10 text-heading-3 font-semibold leading-tight opacity-0 2xl:max-w-3xl 2xl:text-5xl"
           >
             Got a vision? Together, we'll make it shine in the digital world.
           </h3>
           <p
             ref={body}
-            className="max-w-md mt-4 translate-y-10 opacity-0 text-body-2 text-accent-100 2xl:max-w-2xl 2xl:text-3xl"
+            className="mt-4 max-w-md translate-y-10 text-body-2 text-accent-100 opacity-0 2xl:max-w-2xl 2xl:text-3xl"
           >
             If you're looking to turn that 'aha!' moment into an 'oh wow!'
             reality, I'm your huckleberry. Currently accepting sidekicks for
             web-venture quests, starting now!
           </p>
-          <form
-            name="contact"
-            action="/contact"
-            autoComplete="off"
-            // eslint-disable-next-line react/no-unknown-property
-            className="mt-10 font-grotesk"
-            method="POST"
-          >
-            <input type="hidden" name="form-name" value="contact" />
-            <div className="grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2">
-              <div className="relative z-0">
-                <input
-                  required
-                  type="text"
-                  id="name"
-                  name="name"
-                  className="peer block w-full appearance-none border-0 border-b border-accent-100 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="name"
-                  className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 2xl:text-body-2"
-                >
-                  Your name
-                </label>
-              </div>
-              <div className="relative z-0">
-                <input
-                  required
-                  type="text"
-                  name="email"
-                  id="email"
-                  className="peer block w-full appearance-none border-0 border-b border-accent-100 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
-                  placeholder=" "
-                />
-                <label
-                  htmlFor="email"
-                  className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 2xl:text-body-2"
-                >
-                  Your email
-                </label>
-              </div>
-              <div className="relative z-0 sm:col-span-2">
-                <textarea
-                  required
-                  id="message"
-                  name="message"
-                  rows="2"
-                  className="peer block w-full resize-none appearance-none border-0 border-b border-accent-100 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
-                  placeholder=" "
-                ></textarea>
-                <label
-                  htmlFor="message"
-                  className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 2xl:text-body-2"
-                >
-                  Your message
-                </label>
-              </div>
-            </div>
-            <button
-              type="submit"
-              className="mt-10 duration-200 border button group hover:border-accent-400 hover:bg-transparent"
+          {state.succeeded ? (
+            <p className="mt-10 font-grotesk text-body-1 font-semibold">
+              Thanks for reaching out! You should receive a response soon.
+            </p>
+          ) : (
+            <form
+              name="contact"
+              autoComplete="off"
+              // eslint-disable-next-line react/no-unknown-property
+              className="mt-10 font-grotesk"
+              onSubmit={handleSubmit}
             >
-              <span className="relative">
-                <span className="absolute w-0 h-1 duration-300 ease-out bottom-2 bg-secondary-700 opacity-90 group-hover:w-full"></span>
-                <span className="group-hover:text-accent-400">
-                  Send Message
+              <input type="hidden" name="form-name" value="contact" />
+              <div className="grid grid-cols-1 gap-x-6 gap-y-12 sm:grid-cols-2">
+                <div className="relative z-0">
+                  <input
+                    required
+                    type="text"
+                    id="name"
+                    name="name"
+                    className="peer block w-full appearance-none border-0 border-b border-accent-100 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                  />
+                  <label
+                    htmlFor="name"
+                    className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 2xl:text-body-2"
+                  >
+                    Your name
+                  </label>
+                </div>
+                <div className="relative z-0">
+                  <input
+                    required
+                    type="email"
+                    name="email"
+                    id="email"
+                    className="peer block w-full appearance-none border-0 border-b border-accent-100 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                  />
+                  <ValidationError
+                    prefix="Email"
+                    field="email"
+                    errors={state.errors}
+                  />
+                  <label
+                    htmlFor="email"
+                    className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 2xl:text-body-2"
+                  >
+                    Your email
+                  </label>
+                </div>
+                <div className="relative z-0 sm:col-span-2">
+                  <textarea
+                    required
+                    id="message"
+                    name="message"
+                    rows="2"
+                    className="peer block w-full resize-none appearance-none border-0 border-b border-accent-100 bg-transparent px-0 py-2.5 focus:outline-none focus:ring-0"
+                    placeholder=" "
+                  ></textarea>
+                  <ValidationError
+                    prefix="Message"
+                    field="message"
+                    errors={state.errors}
+                  />
+                  <label
+                    htmlFor="message"
+                    className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-body-3 text-secondary-600 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 2xl:text-body-2"
+                  >
+                    Your message
+                  </label>
+                </div>
+              </div>
+              <button
+                type="submit"
+                className="button group mt-10 border duration-200 hover:border-accent-400 hover:bg-transparent"
+              >
+                <span className="relative">
+                  <span className="absolute bottom-2 h-1 w-0 bg-secondary-700 opacity-90 duration-300 ease-out group-hover:w-full"></span>
+                  <span className="group-hover:text-accent-400">
+                    <button
+                      type="submit"
+                      disabled={state.submitting}
+                      className="disabled:cursor-not-allowed"
+                    >
+                      Send message
+                    </button>
+                  </span>
                 </span>
-              </span>
-            </button>
-          </form>
+              </button>
+            </form>
+          )}
         </div>
-        <div className="grid grid-cols-1 col-span-2 gap-x-4 gap-y-8 text-accent-300 sm:grid-cols-2 sm:gap-y-0 md:grid-cols-1">
+        <div className="col-span-2 grid grid-cols-1 gap-x-4 gap-y-8 text-accent-300 sm:grid-cols-2 sm:gap-y-0 md:grid-cols-1">
           <div className="space-y-3 ">
-            <h4 className="font-semibold text-body-1 2xl:text-4xl">
+            <h4 className="text-body-1 font-semibold 2xl:text-4xl">
               Contact Details
             </h4>
             <div className="flex flex-col space-y-3 text-body-2 2xl:text-3xl">
               <a
                 href="mailto:djeddyedward@gmail.com"
-                className="relative cursor-pointer group w-fit"
+                className="group relative w-fit cursor-pointer"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -161,13 +184,13 @@ export default function Contact({ hashRef }) {
             </div>
           </div>
           <div className="space-y-3 ">
-            <h4 className="font-semibold text-body-1 2xl:text-4xl">
+            <h4 className="text-body-1 font-semibold 2xl:text-4xl">
               My Digital Spaces
             </h4>
             <div className="space-y-3 text-body-2 2xl:text-3xl">
               <a
                 href="https://github.com/Kayeddy"
-                className="flex items-center space-x-2 group"
+                className="group flex items-center space-x-2"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -179,7 +202,7 @@ export default function Contact({ hashRef }) {
               </a>
               <a
                 href="https://www.linkedin.com/in/edushed/"
-                className="flex items-center space-x-2 group w-fit"
+                className="group flex w-fit items-center space-x-2"
                 target="_blank"
                 rel="noreferrer"
               >
@@ -192,7 +215,7 @@ export default function Contact({ hashRef }) {
             </div>
           </div>
           <div className="space-y-3 ">
-            <h4 className="font-semibold text-body-1 2xl:text-4xl">Location</h4>
+            <h4 className="text-body-1 font-semibold 2xl:text-4xl">Location</h4>
             <div className="space-y-2 text-body-2 2xl:text-3xl">
               <p>
                 Medellín, Colombia <br></br>
