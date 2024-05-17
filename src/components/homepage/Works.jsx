6ff085738de5d76b7700e6e4a4cdb84a2920431d
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2024-05-16 19:09:38 -0500

    refactor(works): Add an additional work to portfolio

--- src/components/homepage/Works.jsx
+++ src/components/homepage/Works.jsx
@@ -1,8 +1,10 @@
 /* eslint-disable react/prop-types */
 
 import throu from "/src/assets/images/throu.webp";
+import gdc from "/src/assets/images/gdc.webp";
 import threadsy from "/src/assets/images/threadsy.webp";
-import acc from "/src/assets/images/acc.webp";
+import c2p_launch from "/src/assets/images/c2p_launch.webp";
+
 import Projects from "../ui/Projects";
 import Heading from "../ui/Heading";
 
@@ -27,7 +29,34 @@ export default function Works({ forwardedRef, hashRef }) {
               tools="ReactJS • TailwindCSS • JavaScript • Vite"
             />
           </div>
+
           {/* Project #2 */}
+          <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
+            <Projects
+              link="globaldeveloperclubs.com"
+              img={gdc}
+              alt="global developers club website"
+              name="Global developers Club"
+              type="Web Design • Fullstack Development"
+              year="2024"
+              tools="Figma • NextJs • Typescript • TailwindCSS • Clerk • MongoDB"
+            />
+          </div>
+
+          {/* Project #3 */}
+          <div className="col-span-1 h-fit md:col-span-4">
+            <Projects
+              link="https://connect2pet.app"
+              img={c2p_launch}
+              alt="connect2pet launch website"
+              name="connect 2 pet launch website"
+              type="Web Design • Frontend Development"
+              year="2024"
+              tools="ReactJS • SCSS • Figma • Vite"
+            />
+          </div>
+
+          {/* Project #4 */}
           <div className="col-span-1 pt-0 md:col-span-7 md:pt-16">
             <Projects
               link="https://threads-clone-web.vercel.app"
@@ -36,48 +65,26 @@ export default function Works({ forwardedRef, hashRef }) {
               name="personal threads clone website"
               type="Fullstack Development"
               year="2023"
-              tools="ReactJS • NextJs • Typescript • TailwindCSS"
-            />
-          </div>
-          {/* Project #3 */}
-          <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
-            <Projects
-              link="https://connect2pet.app"
-              img={acc}
-              alt="connect2pet launch website"
-              name="connect 2 pet launch website"
-              type="Web Design • Frontend Development"
-              year="2024"
-              tools="ReactJS • SCSS • Figma"
+              tools="NextJs • Typescript • TailwindCSS • Clerk • MongoDB"
             />
           </div>
 
           {/* <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
             <Projects
-              link="https://realbusinessaccountants.netlify.app"
-              img={acc}
-              alt="real business accountant project mockup"
-              name="real business accountants"
+              link="https://connect2pet.app"
+              img={c2p_launch}
+              alt="connect2pet launch website"
+              name="connect 2 pet launch website"
               type="Web Design • Frontend Development"
-              year="2023"
-              tools="HTML • CSS • JavaScript • Figma"
+              year="2024"
+              tools="ReactJS • SCSS • Figma • Vite"
             />
-          </div>
-          <div className="col-span-1 pt-0 h-fit md:col-span-8 md:pt-20">
-            <Projects
-              link="https://godaddyuiclone.netlify.app"
-              img={daddy}
-              alt="godaddy clone page mockup"
-              name="GODADDY LANDING PAGE CLONE"
-              type="Frontend Development"
-              year="2023"
-              tools="HTML • TailwindCSS • JavaScript"
-            />
-          </div>
-          <div className="col-span-1 h-fit md:col-span-4">
+          </div> */}
+
+          {/* <div className="col-span-1 h-fit md:col-span-4">
             <Projects
               link="https://sunnysidechallenge.netlify.app"
-              img={sunnyside}
+              img={c2p_launch}
               alt="sunnyside project mockup"
               name="SUNNYSIDE LANDING PAGE"
               type="Frontend Development"
