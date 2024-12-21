78b16c8b9c3ef256991a153df0103fc5864b0022
Edward Anthony Escudero Bowie <57167218+Kayeddy@users.noreply.github.com>
2024-12-21 02:18:09 -0500

    refactor(works): Updated work list

--- src/components/homepage/Works.jsx
+++ src/components/homepage/Works.jsx
@@ -4,9 +4,7 @@ import throu from "/src/assets/images/throu.webp";
 import gdc from "/src/assets/images/gdc.webp";
 // import threadsy from "/src/assets/images/threadsy.webp";
 import c2p_launch from "/src/assets/images/c2p_launch.webp";
-import maski from "/src/assets/images/maski.webp";
 import gunny from "/src/assets/images/gunny-games.webp";
-import gunnyGacha from "/src/assets/images/gunny-gacha.webp";
 
 import Projects from "../ui/Projects";
 import Heading from "../ui/Heading";
@@ -35,22 +33,7 @@ export default function Works({ forwardedRef, hashRef }) {
           </div>
 
           {/* Project #2 */}
-          <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
-            <Projects
-              link="https://maski.pet"
-              img={maski}
-              alt="Maski_official_website"
-              name="Maski"
-              type="Web Design • Fullstack Development"
-              year="2024"
-              tools="Figma • NextJs • Typescript • TailwindCSS • MongoDB"
-              description="Tinder-like app designed to make pet adoption an
-          enjoyable and intuitive experience. Swipe through profiles of adorable
-          pets, just like you would on a dating app, to find the perfect match."
-            />
-          </div>
 
-          {/* Project #3 */}
           <div className="col-span-1 h-fit md:col-span-4">
             <Projects
               link="https://connect2petlaunch-kayeddys-projects.vercel.app/"
@@ -64,7 +47,7 @@ export default function Works({ forwardedRef, hashRef }) {
             />
           </div>
 
-          {/* Project #4 */}
+          {/* Project #3 */}
 
           <div className="col-span-1 pt-0 md:col-span-7 md:pt-16">
             <Projects
@@ -76,6 +59,20 @@ export default function Works({ forwardedRef, hashRef }) {
               year="2024"
               tools="NextJs • Typescript • TailwindCSS • SASS • Figma"
               description="Gunny Games Studios is a pioneering video game development company that integrates blockchain technology to deliver an enhanced and unique user experience."
+            />
+          </div>
+
+          {/* Project #4 */}
+          <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
+            <Projects
+              link="https://globaldeveloperclubs.com"
+              img={gdc}
+              alt="global_developers_club_official_website"
+              name="GlOBAL DEVELOPERS CLUB"
+              type="Web Design • Fullstack Development "
+              year="2023"
+              tools="NextJs • Typescript • TailwindCSS • MongoDB"
+              description="At the Global Developers Club, we provide exclusive resources and support from our partners to help you and your co-founders or team members take your project to the next level. From the initial idea to a fully developed product, we are here to assist you every step of the way."
             />
           </div>
 
@@ -104,19 +101,20 @@ export default function Works({ forwardedRef, hashRef }) {
               description="Gunny Games Gacha is Gunny Games' official platform, where users can win prizes and earn HERO tokens, the company’s official cryptocurrency."
             />
           </div> */}
-
-          <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
+          {/* <div className="col-span-1 h-fit pt-0 md:col-span-8 md:pt-20">
             <Projects
-              link="https://globaldeveloperclubs.com"
-              img={gdc}
-              alt="global_developers_club_official_website"
-              name="GlOBAL DEVELOPERS CLUB"
-              type="Web Design • Fullstack Development "
-              year="2023"
-              tools="NextJs • Typescript • TailwindCSS • MongoDB"
-              description="At the Global Developers Club, we provide exclusive resources and support from our partners to help you and your co-founders or team members take your project to the next level. From the initial idea to a fully developed product, we are here to assist you every step of the way."
+              link="https://maski.pet"
+              img={maski}
+              alt="Maski_official_website"
+              name="Maski"
+              type="Web Design • Fullstack Development"
+              year="2024"
+              tools="Figma • NextJs • Typescript • TailwindCSS • MongoDB"
+              description="Tinder-like app designed to make pet adoption an
+          enjoyable and intuitive experience. Swipe through profiles of adorable
+          pets, just like you would on a dating app, to find the perfect match."
             />
-          </div>
+          </div> */}
 
           {/* <div className="col-span-1 pt-0 md:col-span-5 md:pt-80">
             <Projects
